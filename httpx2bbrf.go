ba9a37c6bbc033ff4346f644a1757789dcb0418f
Chase Jensen <soccerchase@gmail.com>
2023-08-19 04:36:14 -0600

    Fix error [ERROR] invalid literal for int() with base 10: '<nil>'

--- httpx2bbrf.go
+++ httpx2bbrf.go
@@ -3,42 +3,45 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 )
 
 func main() {
 	decoder := json.NewDecoder(os.Stdin)
-	
+
 	for {
-		json := make(map[string]interface{})
-		
-		decoder.Decode(&json)
-		
-		urlUrl := json["url"]	
-		urlTitle := json["title"]
-		urlWebserver := json["webserver"]
-                urlContentType := json["content_type"]
-                urlContentLength := json["content_length"]
-                urlStatusCode := json["status_code"]
-                urlServerResponse := json["body"]
-		
+		var jsonMap map[string]interface{}
+
+		if err := decoder.Decode(&jsonMap); err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error decoding JSON:", err)
+			continue
+		}
+
+		urlUrl := jsonMap["url"]
+		urlTitle := jsonMap["title"]
+		urlWebserver := jsonMap["webserver"]
+		urlContentType := jsonMap["content_type"]
+		urlContentLength := jsonMap["content_length"]
+		urlServerResponse := jsonMap["body"]
+
 		urlUrlString := fmt.Sprintf("%v", urlUrl)
 		urlTitleString := fmt.Sprintf("%v", urlTitle)
 		urlWebserverString := fmt.Sprintf("%v", urlWebserver)
 		urlContentTypeString := fmt.Sprintf("%v", urlContentType)
 		urlContentLengthString := fmt.Sprintf("%v", urlContentLength)
-		urlStatusCodeString := fmt.Sprintf("%v", urlStatusCode)
 		urlServerResponseString := fmt.Sprintf("%v", urlServerResponse)
-		
-                if urlUrlString != "<nil>" {
 
-                        fullCommand := "bbrf url add '" + urlUrlString + " " + urlStatusCodeString + " " + urlContentLengthString + "' -t 'title:" + urlTitleString + "'" + " -t 'webserver:" + urlWebserverString + "'" + " -t 'contenttype:" + urlContentTypeString + "'" + " -t 'contentlength:" + urlContentLengthString + "'" + " -t 'statuscode:" + urlStatusCodeString + "'" + " -t 'serverresponse:" + urlServerResponseString + "'" + " -p @INFER"
-                        out, _ :=exec.Command("sh","-c",fullCommand).Output()
-                        fmt.Printf("%s", out)
-
-                } else {
-                        break
-                }
-        }
+		if urlUrlString != "" {
+			fullCommand := "bbrf url add '" + urlUrlString + " " + urlContentLengthString + "' -t 'title:" + urlTitleString + "'" + " -t 'webserver:" + urlWebserverString + "'" + " -t 'contenttype:" + urlContentTypeString + "'" + " -t 'contentlength:" + urlContentLengthString + "'" + " -t 'serverresponse:" + urlServerResponseString + "'" + " -p @INFER"
+			out, _ := exec.Command("sh", "-c", fullCommand).Output()
+			fmt.Printf("%s", out)
+		} else {
+			break
+		}
+	}
 }
